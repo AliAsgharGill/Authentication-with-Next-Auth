fc7b821321c68f0311649bfa616b0652915a747a
AliAsgharGill <stylinalivlogs@gmail.com>
2024-06-26 18:37:03 +0500

    working with github

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth";
+import AppleProvider from "next-auth/providers/apple";
+import FacebookProvider from "next-auth/providers/facebook";
+import GoogleProvider from "next-auth/providers/google";
+import EmailProvider from "next-auth/providers/email";
+import GithubProvider from "next-auth/providers/github";
+
+const handler = NextAuth({
+  // Github provider
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+});
+
+export { handler as GET, handler as POST };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import SessionWrapper from "@/app/components/SessionWrapper";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <SessionWrapper>
+        <body className={inter.className}>{children}</body>
+      </SessionWrapper>
     </html>
   );
 }
