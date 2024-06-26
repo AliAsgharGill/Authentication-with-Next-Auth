1a0396460a04d82ff9de4934942011e33149f4da
AliAsgharGill <stylinalivlogs@gmail.com>
2024-06-26 19:00:33 +0500

    minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useSession, signIn, signOut } from "next-auth/react";
+import Image from "next/image";
 
 export default function Component() {
   const { data: session } = useSession();
@@ -7,15 +8,32 @@ export default function Component() {
   if (session) {
     return (
       <>
-        Signed in as {session.user?.email ?? "unknown"} <br />
-        <button onClick={() => signOut()}>Sign out</button>
+        <div className="flex justify-around items-center space-x-3 w-1/4 p-5 ">
+          <Image
+            className="rounded-full"
+            priority={false}
+            src={session.user?.image ?? "No Image"}
+            alt="Description of image"
+            width={50}
+            height={50}
+          />
+          Signed in as {session.user?.email ?? "unknown"} <br />
+          <div>
+            <button onClick={() => signOut()}>Sign out</button>
+          </div>
+        </div>
       </>
     );
-  }      
+  }
   return (
     <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
+      <div className="flex justify-around items-center space-x-3 w-1/4 p-5 ">
+        Not signed in <br />
+        <button onClick={() => signIn()}>Sign in</button>
+        {/* if we want to login directly by click on button then write name of provider e.g. google, github in signIn() as parameter */}
+        {/* <button onClick={() => signIn("google")}>Sign in with Google</button>
+      <button onClick={() => signIn("github")}>Sign in with Github</button> */}
+      </div>
     </>
   );
 }
