500f838aefc7879013313afb2abc9c043e5b2139
AliAsgharGill <stylinalivlogs@gmail.com>
2024-07-03 13:41:04 +0500

    Access token & Id token Generated

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -17,6 +17,32 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      // Access account object here
+      console.log('Account Object:', account);
+      // You can perform actions like saving user to database
+      return true;
+    },
+    async redirect({ url, baseUrl }) {
+      return url.startsWith(baseUrl) ? url : baseUrl;
+    },
+    async session({ session, token, user }) {
+      // Modify session object
+      session.user.id = token.id;
+      return session;
+    },
+    async jwt({ token, user, account, profile, isNewUser }) { 
+      // Initial sign in
+      if (account && user) {
+        token.accessToken = account.accessToken;
+        token.idToken = account.idToken;
+        token.refreshToken = account.refreshToken;
+        token.providerAccountId = account.providerAccountId;
+      }
+      return token;
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
